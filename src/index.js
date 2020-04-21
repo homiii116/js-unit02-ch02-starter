3af5d4a0a88560818f5ff031dce05b080e75cb3e
miho yamagata <miho.yamagata@gmail.com>
2020-04-21 22:04:50 +0900

    submit

--- src/index.js
+++ src/index.js
@@ -16,35 +16,46 @@ const propertyData = {
 /* 
   getDataを呼び出して、mainEl.innerHTMLを利用して、結果を出力します。
 */
-// function handleClick(e) {
-//   e.preventDefault();
-//   const mainEl = document.getElementById('main');
+function handleClick(e) {
+  e.preventDefault();
+  const mainEl = document.getElementById('main');
   
-//   getData()
-//     .then((result) => {      
-//       mainEl.innerHTML = result;
-//       document.body = mainEl;
-//     })
+  return getData()
+    .then((result) => {      
+      mainEl.innerHTML = `
+      <div className="property-info-wrapper">
+        <p>タイトル：${result.propertyName}</p>
+        <p>部屋の種類：${result.propertyType}</p>
+        <p>キャンセルポリシー：${result.cancelPolicy}</p>
+        <p>部屋数：${result.roomNum}</p>
+        <p>浴室：${result.bathroomNum}</p>
+        <p>料金（ドル）：${result.priceInDollars}</p>
+        <p>ホスト名：${result.host.firstName}</p>
+      </div>
+      `
+    })
 
-//     .catch((error) => {
-//       mainEl.innerHTML = error;
-//       document.body = mainEl;
-//     })
-// }
+    .catch((error) => {
+      mainEl.innerHTML = `
+      <div className="property-info-wrapper">
+        <p>${error.message}</p>
+      </div>
+      `
+    })
+}
 /* 
   fetchDataを呼び出して、戻ってきたデータのsuccessの値を元にresolveで物件データまたは、rejectでエラーメッセージを返す。
 */
 
 function getData(){　
-  fetchData().then((data) => {
+  return fetchData().then((data) => {
     if (data.success) {
-      resolve(data.propertyData);
+      return Promise.resolve(data.propertyData);
     } else {
-      reject(data.message);
+      return Promise.reject(data.message);
     }
   });
 }
-getData();
 
 /* 
   lodashのrandom()を使って、80%の確率で正しいデータを返し、20%の確率でエラーを返すようにしましょう。
@@ -69,7 +80,7 @@ function fetchData() {
   });
 }
 
-// {
-//   const button1 = document.getElementById('button1');
-//   button1.addEventListener("click", handleClick);
-// }
+{
+  const button1 = document.getElementById('button1');
+  button1.addEventListener("click", handleClick);
+}
